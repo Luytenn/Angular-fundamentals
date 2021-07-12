4c52552fca25e2ce622ff0c30ad43d2201af721d
efrain <efrain.yauri@hotmail.com>
2021-07-11 19:07:56 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+/* Crear un módulo llamado ContadorModule */
+// declaraciones, y exportanciones
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
